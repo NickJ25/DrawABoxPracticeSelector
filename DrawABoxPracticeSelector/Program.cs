9a1505a134ae9d205528a42bda9c82f3e6b0bd81
NickJ25 <35298197+NickJ25@users.noreply.github.com>
2022-11-05 13:36:29 -0800

    Add project files.

---
+++ DrawABoxPracticeSelector/Program.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+//using IHost host = Host.CreateDefaultBuilder(args)
+//    .ConfigureServices((_, services) =>
+//    services.AddTransient<>())
+//    .Build();
+
+//await host.RunAsync();
